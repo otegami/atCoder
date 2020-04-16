e6c40b0139c373eaf64723ac4b032d03035d40c3
otegami <a.s.takuya1026@gmail.com>
2020-04-16 09:36:37 +0900

    [review] Anout bit

--- Practice/antBook/aLotOf.rb
+++ Practice/antBook/aLotOf.rb
@@ -1,21 +1,6 @@
-s = gets.chomp
-t = s.to_s.size - 1
-sum = 0
-
-(2 ** t).times do |i|
-  temp = s.dup
-  t.times do |j|
-    k = s.size - temp.size
-    if i[j] == 1
-      sum += temp.slice!(0, j - k + 1).to_i
-    end
-  end
-  sum += temp.to_i
-end
-puts sum
-
 s=gets.chomp
 r=0
+# I will want to use bit thinking!!
 (1<<(s.size-1)).times do |i|
 	x=0
 	s.size.times do |j|
